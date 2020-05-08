fe686c8629429d0b7558cbdc57a3cad0ce1e0443
bhban <needleworm@kaist.ac.kr>
2020-05-08 11:38:10 +0900

    Update

---
+++ ion_preprocesing/calibration.py
@@ -0,0 +1,26 @@
+from scipy import stats
+
+
+class Exp:
+    def __init__(self, filename):
+
+        pass
+
+    def readjust(self, raw_value):
+        return
+
+
+class ExpExp:
+    def __init__(self, filename):
+        pass
+
+    def readjust(self, raw_value):
+        return
+
+
+class DeepLearning:
+    def __init__(self, filename):
+        pass
+
+    def readjust(self, raw_value):
+        return
