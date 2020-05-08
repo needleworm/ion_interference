e02a7f90411a87d3b669a484a334a61d04db5545
bhban <needleworm@kaist.ac.kr>
2020-05-08 15:47:44 +0900

    Deeplearning Done

--- ion_preprocesing/calibration.py
+++ ion_preprocesing/calibration.py
@@ -54,7 +54,7 @@ class Exp:
         self.label_header, self.label = csv_into_array(label_filename)
 
         if self.data.shape != self.label.shape:
-            print("The data and label has different length.")
+            print("The data and label has different shape.")
             exit(1)
 
         self.data_size, self.data_category = self.data.shape
@@ -86,7 +86,7 @@ class Exp:
         header, data = csv_into_array(filename)
         for i in range(len(self.equation)):
             data[:, i] = self.equation[i].calculate(data[:, i])
-        np.savetxt("converted_" + filename, delimiter=", ", header=header)
+        np.savetxt("converted_" + filename, data, delimiter=", ", header=header)
 
 
 class ExpExp:
@@ -95,7 +95,7 @@ class ExpExp:
         self.label_header, self.label = csv_into_array(label_filename)
 
         if self.data.shape != self.label.shape:
-            print("The data and label has different length.")
+            print("The data and label has different shape.")
             exit(1)
 
         self.data_size, self.data_category = self.data.shape
@@ -130,18 +130,79 @@ class ExpExp:
         header, data = csv_into_array(filename)
         for i in range(len(self.equation)):
             data[:, i] = self.equation[i].calculate(data[:, i])
-        np.savetxt("converted_" + filename, delimiter=", ", header=header)
+        np.savetxt("converted_" + filename, data, delimiter=", ", header=header)
 
 
 class DeepLearning:
     def __init__(self, data_filename, label_filename):
+        self.bathc_size = 128
+        self.epoch = 10
+        self.device = "/gpu:0"
+
         self.data_header, self.data = csv_into_array(data_filename)
         self.label_header, self.label = csv_into_array(label_filename)
 
-        if self.data.shape != self.label.shape:
+        self.data_length, self.data_size = self.data.shape
+        self.label_length, self.label_size = self.label.shape
+
+        if self.data.length != self.label.length:
             print("The data and label has different length.")
             exit(1)
 
-    def readjust(self, raw_value):
-        return
+        self.log_dir = str(time.time) + "_log"
 
+        self.division_x = np.max(self.data)
+        self.division_y = np.max(self.label)
+
+        self.data /= self.division_x
+        self.label /= self.division_y
+
+        self.test_data = self.data[: int(self.data_length / 5)]
+        self.train_data = self.data[int(self.data_length / 5):]
+
+        self.test_label = self.label[:int(self.label_length/ 5)]
+        self.train_label = self.label[int(self.label_length / 5):]
+
+        self.tensorboard = keras.callbacks.TensorBoard(log_dir=self.log_dir + "/{}".format(time.time()))
+
+        with tf.device(self.device):
+            self.Graph = self.graph(True, self.log_dir)
+
+        self.Graph.fit(self.train_data, self.train_label,
+                       epoch=self.epoch,
+                       batch_size=self.batch_size,
+                       callbacks=[self.tensorboard],
+                       validation_data=(self.test_data, self.test_label))
+
+        self.test_loss, self.test_acc = self.Graph.evaluate(self.test_data, self.test_label)
+        print("** test loss is : " + str(self.test_loss))
+        print("** test acc is : " + str(self.test_acc))
+
+        print("** Training Done **")
+        print(self.Graph.summary())
+
+        self.Graph.save(self.log_dir + "/saved_model.h5")
+
+    def graph(self, reset, logdir):
+        if reset:
+            model = tf.keras.Sequential()
+            model.add(layers.Dense(60, activation="relu"))
+            model.add(layers.BatchNormalization())
+            model.add(layers.Dense(60, activation="relu"))
+            model.add(layers.BatchNormalization())
+            model.add(layers.Dense(30, activation="relu"))
+            model.add(layers.BatchNormalization())
+            model.add(layers.Dense(20, activation="relu"))
+            model.add(layers.BatchNormalization())
+            model.add(layers.Dense(self.Y_size, activation="sigmoid"))
+            model.compile(optimizer=self.optimizer,
+                          loss=self.loss,
+                          metrics=self.metrics)
+        else:
+            model = keras.models.load_model(logdir + "/saved_model.h5")
+        return model
+
+    def volt_to_concentration(self, filename):
+        header, data = csv_into_array(filename)
+        result = self.Graph.predict(data, batch_size=self.batch_size)
+        np.savetxt("converted_" + filename, result, delimiter=", ", header=header)
--- ion_preprocesing/readjustment.py
+++ ion_preprocesing/readjustment.py
@@ -53,16 +53,13 @@ class Linear:
         self.label_header, self.label = csv_into_array(label_filename)
 
         if self.data.shape != self.label.shape:
-            print("The data and label has different length.")
+            print("The data and label has different shape.")
             exit(1)
 
         self.data_size, self.data_category = self.data.shape
 
         self.slopes = []
         self.intercepts = []
-        self.r_values = []
-        self.p_values = []
-        self.std_errs = []
 
         self.regression(self.data, self.label) # x y 모두 그대로 선형회귀
         self.equation = make_equation("linear", self.slopes, self.intercepts)
@@ -88,7 +85,7 @@ class Linear:
         header, data = csv_into_array(filename)
         for i in range(len(self.equation)):
             data[:, i] = self.equation[i].calculate(data[:, i])
-        np.savetxt("converted_" + filename, delimiter=", ", header=header)
+        np.savetxt("converted_" + filename, data, delimiter=", ", header=header)
 
 
 class Quadratic:
@@ -97,16 +94,13 @@ class Quadratic:
         self.label_header, self.label = csv_into_array(label_filename)
 
         if self.data.shape != self.label.shape:
-            print("The data and label has different length.")
+            print("The data and label has different shape.")
             exit(1)
 
         self.data_size, self.data_category = self.data.shape
 
         self.slopes = []
         self.intercepts = []
-        self.r_values = []
-        self.p_values = []
-        self.std_errs = []
 
         self.quadratic_regression(self.data, self.label) # x y 둘다 그대로 quadratic regression
         self.equation = make_equation("quadratic", self.slopes, self.intercepts)
@@ -132,18 +126,79 @@ class Quadratic:
         header, data = csv_into_array(filename)
         for i in range(len(self.equation)):
             data[:, i] = self.equation[i].calculate(data[:, i])
-        np.savetxt("converted_" + filename, delimiter=", ", header=header)
+        np.savetxt("converted_" + filename, data, delimiter=", ", header=header)
 
 
 class DeepLearning:
     def __init__(self, data_filename, label_filename):
+        self.bathc_size = 128
+        self.epoch = 10
+        self.device = "/gpu:0"
+
         self.data_header, self.data = csv_into_array(data_filename)
         self.label_header, self.label = csv_into_array(label_filename)
 
-        if self.data.shape != self.label.shape:
+        self.data_length, self.data_size = self.data.shape
+        self.label_length, self.label_size = self.label.shape
+
+        if self.data.length != self.label.length:
             print("The data and label has different length.")
             exit(1)
 
-    def readjust(self, raw_value):
-        return
+        self.log_dir = str(time.time) + "_log"
 
+        self.division_x = np.max(self.data)
+        self.division_y = np.max(self.label)
+
+        self.data /= self.division_x
+        self.label /= self.division_y
+
+        self.test_data = self.data[: int(self.data_length / 5)]
+        self.train_data = self.data[int(self.data_length / 5):]
+
+        self.test_label = self.label[:int(self.label_length/ 5)]
+        self.train_label = self.label[int(self.label_length / 5):]
+
+        self.tensorboard = keras.callbacks.TensorBoard(log_dir=self.log_dir + "/{}".format(time.time()))
+
+        with tf.device(self.device):
+            self.Graph = self.graph(True, self.log_dir)
+
+        self.Graph.fit(self.train_data, self.train_label,
+                       epoch=self.epoch,
+                       batch_size=self.batch_size,
+                       callbacks=[self.tensorboard],
+                       validation_data=(self.test_data, self.test_label))
+
+        self.test_loss, self.test_acc = self.Graph.evaluate(self.test_data, self.test_label)
+        print("** test loss is : " + str(self.test_loss))
+        print("** test acc is : " + str(self.test_acc))
+
+        print("** Training Done **")
+        print(self.Graph.summary())
+
+        self.Graph.save(self.log_dir + "/saved_model.h5")
+
+    def graph(self, reset, logdir):
+        if reset:
+            model = tf.keras.Sequential()
+            model.add(layers.Dense(60, activation="relu"))
+            model.add(layers.BatchNormalization())
+            model.add(layers.Dense(60, activation="relu"))
+            model.add(layers.BatchNormalization())
+            model.add(layers.Dense(30, activation="relu"))
+            model.add(layers.BatchNormalization())
+            model.add(layers.Dense(20, activation="relu"))
+            model.add(layers.BatchNormalization())
+            model.add(layers.Dense(self.Y_size, activation="sigmoid"))
+            model.compile(optimizer=self.optimizer,
+                          loss=self.loss,
+                          metrics=self.metrics)
+        else:
+            model = keras.models.load_model(logdir + "/saved_model.h5")
+        return model
+
+    def volt_to_concentration(self, filename):
+        header, data = csv_into_array(filename)
+        result = self.Graph.predict(data, batch_size=self.batch_size)
+        np.savetxt("converted_" + filename, result, delimiter=", ", header=header)
